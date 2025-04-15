d7a3fc0a561319af5084bf408f7cb425a925e503
Robin Vloeberghs <u0141056@kuleuven.be>
2025-04-15 15:54:29 +0200

    slight increase a_max

--- hmfc/constants.py
+++ hmfc/constants.py
@@ -1,7 +1,7 @@
 """Constants of the model.
 """
 
-A_MAX = 0.995 # upper limit for a_i
+A_MAX = 0.99 # upper limit for a_i
 SIGMA_A_MAX = 0.2 # upper limit for standard deviation of truncated normal for a_i
 PG_TRUNC = 200 # number of gamma samples for PG sample
 SIGMASQ0 = 1.0 # error variance x on first trial
